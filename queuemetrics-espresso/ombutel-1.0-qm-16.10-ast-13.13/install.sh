7d3438d3fdabda02c508ae56fae6435554b6cefe
mirkotp <mirkotp@users.noreply.github.com>
2017-04-20 09:45:27 +0200

    Update reference to external logo

--- queuemetrics-espresso/ombutel-1.0-qm-16.10-ast-13.13/install.sh
+++ queuemetrics-espresso/ombutel-1.0-qm-16.10-ast-13.13/install.sh
@@ -64,7 +64,7 @@ rv $CPROPSQM realtime.agent_button_3.enabled false
 rv $CPROPSQM realtime.agent_button_4.enabled false
 
 rv $CPROPSQM default.monitored_calls /var/spool/asterisk/monitor/
-rv $CPROPSQM layout.logo http://www.loway.ch/images/logo_ombutel_200x72.png
+rv $CPROPSQM layout.logo https://www.loway.ch/img/customlogos/ombutel-logo.png
 
 rv $CPROPSQM realtime.members_only false
 rv $CPROPSQM realtime.refresh_time 10
